1517dacdfb81ac32d5bf1aecd71cd1ac1cc05284
ximofam <vienpham177@gmail.com>
2026-06-26 22:26:33 +0700

    add api create comment

--- src/main/java/com/ximofam/graduation_project/forums/controllers/ApiCommentController.java
+++ src/main/java/com/ximofam/graduation_project/forums/controllers/ApiCommentController.java
@@ -1,16 +1,15 @@
 package com.ximofam.graduation_project.forums.controllers;
 
+import com.ximofam.graduation_project.forums.dtos.request.CreateCommentRequest;
 import com.ximofam.graduation_project.forums.dtos.response.CommentResponse;
 import com.ximofam.graduation_project.forums.services.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/comments")
@@ -24,5 +23,10 @@ public class ApiCommentController {
             @PageableDefault(size = 10) Pageable pageable) {
 
         return ResponseEntity.ok(commentService.getReplyComments(id, pageable));
+    }
+
+    @PostMapping
+    public ResponseEntity<CommentResponse> createComment(@RequestBody CreateCommentRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(commentService.createComment(request));
     }
 }
