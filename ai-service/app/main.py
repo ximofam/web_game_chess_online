a5fc8e754a48a8337d6e7baf819bd4ca77f72bf3
ximofam <vienpham177@gmail.com>
2026-08-12 23:42:26 +0700

    feat: init health check apij

--- ai-service/app/main.py
+++ ai-service/app/main.py
@@ -1,1 +1,6 @@
-print("Hello World by Ximofam")
+from fastapi import FastAPI
+from app.api.router import api_router
+
+app = FastAPI(title="AI Service API")
+
+app.include_router(api_router, prefix="/api")
